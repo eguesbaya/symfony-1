4b0517313ac8546d71e970d5f989086580a86758
eguesbaya <guesbaya@hotmail.fr>
2021-06-07 11:16:29 +0200

    10 - Symfony: Les relations bidirectionnelles

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,10 @@ use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\SeasonRepository;
+use App\Repository\ProgramRepository;
+use App\Entity\Season;
+use App\Entity\Episode;
 
 /**
  * @Route("/programs", name="program_")
@@ -44,12 +48,10 @@ class ProgramController extends AbstractController
 
  */
 
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
     {
 
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
+        $program = $programRepository->findOneBy(['id' => $id]);
 
         if (!$program) {
 
@@ -59,9 +61,36 @@ class ProgramController extends AbstractController
 
         }
 
+        $seasons = $seasonRepository->findAll();
+
         return $this->render('programs/show.html.twig', [
-            'program' => $program,
+            'program' => $program,'seasons' => $seasons
         ]);
+    }
+
+
+    /**
+    * @Route("/{programId}/seasons/{seasonId}", name="season_show")
+    * @return Response
+    */
+
+    public function showSeason(int $programId, int $seasonId){
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->find($programId);
+
+        $season = $this->getDoctrine()->getRepository(Season::class)
+            ->find($seasonId);
+
+        $episodes = $this->getDoctrine()
+            ->getRepository(Episode::class)
+            ->findAll();
+
+        return $this->render(
+            'programs/season_show.html.twig',
+            ['program' => $program, 'season' => $season, 'episodes' => $episodes]
+        );
+
     }
 
 }
